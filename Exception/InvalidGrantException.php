8b72a7ffe638f9b693393a0cd64d89ffc1f737ee
Wong Hoi Sing Edison <hswong3i@gmail.com>
2013-05-03 02:48:30 +0800

    * Add request/response/exception class placeholders.

---
+++ Exception/InvalidGrantException.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the pantarei/oauth2-bundle package.
+ *
+ * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pantarei\Bundle\Oauth2Bundle\Exception;
+
+/**
+ * InvalidGrantException
+ *
+ * @author Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
+ */
+class InvalidGrantException extends \InvalidGrantException implements Exception
+{
+}
