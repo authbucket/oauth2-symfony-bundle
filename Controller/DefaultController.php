0deba5e6b1560780d6d56be7e94016e05de20382
Wong Hoi Sing Edison <hswong3i@gmail.com>
2013-05-02 12:14:52 +0800

    * Add demo endpoints for development.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,7 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function authorizeAction($name = 'authorize')
+    {
+        return $this->render('PantareiOauth2Bundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function tokenAction($name = 'token')
+    {
+        return $this->render('PantareiOauth2Bundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function resourceAction($name = 'resource')
     {
         return $this->render('PantareiOauth2Bundle:Default:index.html.twig', array('name' => $name));
     }
