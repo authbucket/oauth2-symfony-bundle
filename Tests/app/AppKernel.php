75379b2318bee56b8ee13f106ed91f0e84709623
Wong Hoi Sing Edison <hswong3i@gmail.com>
2013-06-25 16:47:45 +0800

    Finally, can run phpunit locally!!

---
+++ Tests/app/AppKernel.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Pantarei\Bundle\OAuth2Bundle\OAuth2Bundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This file is part of the pantarei/oauth2-bundle package.
+ *
+ * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = require __DIR__ . '/../vendor/autoload.php';
