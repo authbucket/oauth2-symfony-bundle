144b7a0a0e32ff1e56a01b7ac6b25347f3bdfc3a
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-08 13:27:01 +0800

    Add app/config/config_sami.php.

---
+++ app/config/config_sami.php
@@ -0,0 +1,28 @@
+<?php
+
+use Sami\Sami;
+use Sami\Version\GitVersionCollection;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('Resources')
+    ->exclude('app')
+    ->exclude('build')
+    ->exclude('vendor')
+    ->in($dir = '.');
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('v1.0.*')
+    ->add('master', 'master branch');
+
+return new Sami($iterator, array(
+    'theme' => 'enhanced',
+    'versions' => $versions,
+    'title' => 'AuthBucket\Bundle\OAuth2Bundle API',
+    'build_dir' => __DIR__ . '/../../build/oauth2-bundle/%version%',
+    'cache_dir' => __DIR__ . '/../../build/cache/oauth2-bundle/%version%',
+    'include_parent_data' => false,
+    'default_opened_level' => 2,
+));
