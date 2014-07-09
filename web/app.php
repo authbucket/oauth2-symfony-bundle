c672537737e3ec92758820d5c13f570d98864bac
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-09 11:10:40 +0800

    Enable ApcCache for prod.

--- web/app.php
+++ web/app.php
@@ -8,18 +8,16 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-/*
-$apcLoader = new ApcClassLoader('sf2', $loader);
+$apcLoader = new ApcClassLoader('oauth2-bundle', $loader);
 $loader->unregister();
 $apcLoader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
