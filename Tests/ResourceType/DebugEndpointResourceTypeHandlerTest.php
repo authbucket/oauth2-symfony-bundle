8a8a2196378ad6907fc9ffa96d4bbacf4a6d3600
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-13 17:59:57 +0800

    Issue authbucket/oauth2#3: TODO: Resource Endpoint Firewall Should Check If Required Scope Provided.

---
+++ Tests/ResourceType/DebugEndpointResourceTypeHandlerTest.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * This file is part of the authbucket/oauth2-bundle package.
+ *
+ * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AuthBucket\Bundle\OAuth2Bundle\Tests\ResourceType;
+
+use AuthBucket\Bundle\OAuth2Bundle\Tests\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class DebugEndpointResourceTypeHandlerTest extends WebTestCase
+{
+    public function testExceptionNotExistsAccessToken()
+    {
+        $parameters = array(
+            'debug_token' => "eeb5aa92bbb4b56373b9e0d00bc02d93",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'abcd')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+
+    public function testExceptionExpiredAccessToken()
+    {
+        $parameters = array(
+            'debug_token' => "eeb5aa92bbb4b56373b9e0d00bc02d93",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'd2b58c4c6bc0cc9fefca2d558f1221a5')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+
+    public function testExceptionBadDebugToken()
+    {
+        $parameters = array(
+            'debug_token' => "aaa\x19bbb\x5Cccc\x7Fddd",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+
+    public function testExceptionNotExistsDebugToken()
+    {
+        $parameters = array(
+            'debug_token' => "abcd",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+
+    public function testExceptionExpiredDebugToken()
+    {
+        $parameters = array(
+            'debug_token' => "d2b58c4c6bc0cc9fefca2d558f1221a5",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+
+    public function testExceptionInvalidParameter()
+    {
+        $parameters = array(
+            'debug_token' => 'eeb5aa92bbb4b56373b9e0d00bc02d93',
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint/invalid_options', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('server_error', $resourceResponse['error']);
+    }
+
+    public function testExceptionInvalidClient()
+    {
+        $parameters = array(
+            'debug_token' => 'eeb5aa92bbb4b56373b9e0d00bc02d93',
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint/invalid_client', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('server_error', $resourceResponse['error']);
+    }
+
+    public function testGoodEmptyDebugToken()
+    {
+        $parameters = array();
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('demousername1', $resourceResponse['username']);
+    }
+
+    public function testGoodDebugToken()
+    {
+        $parameters = array(
+            'debug_token' => 'eeb5aa92bbb4b56373b9e0d00bc02d93',
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('demousername1', $resourceResponse['username']);
+    }
+
+    public function testGoodCacheDebugToken()
+    {
+        $parameters = array(
+            'debug_token' => 'eeb5aa92bbb4b56373b9e0d00bc02d93',
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'eeb5aa92bbb4b56373b9e0d00bc02d93')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/debug_endpoint/cache', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('demousername1', $resourceResponse['username']);
+    }
+}
---
+++ Tests/ResourceType/ModelResourceTypeHandlerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of the authbucket/oauth2-bundle package.
+ *
+ * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AuthBucket\Bundle\OAuth2Bundle\Tests\ResourceType;
+
+use AuthBucket\Bundle\OAuth2Bundle\Tests\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class ModelResourceTypeHandlerTest extends WebTestCase
+{
+    public function testExceptionNotExistsAccessToken()
+    {
+        $parameters = array(
+            'debug_token' => "eeb5aa92bbb4b56373b9e0d00bc02d93",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'abcd')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/model', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+
+    public function testExceptionExpiredAccessToken()
+    {
+        $parameters = array(
+            'debug_token' => "eeb5aa92bbb4b56373b9e0d00bc02d93",
+        );
+        $server = array(
+            'HTTP_Authorization' => implode(' ', array('Bearer', 'd2b58c4c6bc0cc9fefca2d558f1221a5')),
+        );
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/resource/debug/model', $parameters, array(), $server);
+        $resourceResponse = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEquals('invalid_request', $resourceResponse['error']);
+    }
+}
