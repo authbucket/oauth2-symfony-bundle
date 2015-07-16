9b2d90c24696dbc1bfad450cedad529f95d13fef
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-07-16 09:18:30 +0800

    Debug Travis CI autoload by NOT using bootstrap.php.cache.

--- app/bootstrap_test.php
+++ app/bootstrap_test.php
@@ -6,4 +6,4 @@ passthru(__DIR__.'/console --env=test -q doctrine:schema:drop --force');
 passthru(__DIR__.'/console --env=test -q doctrine:schema:create');
 passthru(__DIR__.'/console --env=test -q doctrine:fixtures:load -n');
 
-require __DIR__.'/bootstrap.php.cache';
+require __DIR__.'/bootstrap.php';
