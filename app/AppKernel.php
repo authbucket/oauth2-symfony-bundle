c4206dcf84c582f3a4ea81f9003e079ac611e911
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-07-16 12:40:05 +0800

    Cleanup registerBundles() order.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of the authbucket/oauth2-symfony-bundle package.
- *
- * (c) Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -17,15 +8,15 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
-            new AuthBucket\Bundle\OAuth2Bundle\AuthBucketOAuth2Bundle(),
-            new AuthBucket\Bundle\OAuth2Bundle\Tests\TestBundle\TestBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new AuthBucket\Bundle\OAuth2Bundle\AuthBucketOAuth2Bundle(),
+            new AuthBucket\Bundle\OAuth2Bundle\Tests\TestBundle\TestBundle(),
         );
 
         return $bundles;
