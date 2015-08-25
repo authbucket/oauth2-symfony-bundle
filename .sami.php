1c5fe84a346ab10c520465628a5244bb1efc434f
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-08-26 01:05:21 +0800

    Sync with develop.

--- .sami.php
+++ .sami.php
@@ -16,7 +16,7 @@ $versions = GitVersionCollection::create($dir)
     ->addFromTags('*');
 
 return new Sami($iterator, array(
-    'theme' => 'enhanced',
+    'theme' => 'default',
     'versions' => $versions,
     'title' => 'AuthBucket\Bundle\OAuth2Bundle API',
     'build_dir' => __DIR__ . '/build/sami/%version%',
